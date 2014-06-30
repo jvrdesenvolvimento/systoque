ada862439541c3fa788acb3710e16f0c771c954b
João Victor Ramires <jvrdesenvolvimento@gmail.com>
2014-06-30 15:21:13 -0300

    BD 2.0
    Altera as chaves primárias das tabelas e adiciona a coluna perecível na
    tabela produtos

--- bd/systoque.sql
+++ bd/systoque.sql
@@ -28,24 +28,15 @@ CREATE TABLE `item` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `valor` double(9,2) unsigned NOT NULL,
   `quantidade` int(10) unsigned NOT NULL,
-  `produto_cb` varchar(60) NOT NULL,
+  `produto_id` int(10) unsigned NOT NULL,
   `venda_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `produto_cb` (`produto_cb`),
+  KEY `produto_id` (`produto_id`),
   KEY `venda_id` (`venda_id`),
-  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`produto_cb`) REFERENCES `produto` (`cb`),
+  CONSTRAINT `item_ibfk_1` FOREIGN KEY (`produto_id`) REFERENCES `produto` (`id`),
   CONSTRAINT `item_ibfk_2` FOREIGN KEY (`venda_id`) REFERENCES `venda` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `item`
---
-
-LOCK TABLES `item` WRITE;
-/*!40000 ALTER TABLE `item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `produto`
@@ -55,6 +46,7 @@ DROP TABLE IF EXISTS `produto`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `produto` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `cb` varchar(60) NOT NULL,
   `nomeprod` varchar(60) NOT NULL,
   `valorvenda` double(9,2) unsigned NOT NULL,
@@ -63,18 +55,10 @@ CREATE TABLE `produto` (
   `datavalidade` date NOT NULL,
   `prazogarantia` varchar(60) NOT NULL,
   `lote` varchar(60) NOT NULL,
-  PRIMARY KEY (`cb`)
+  `perecivel` varchar(60) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `produto`
---
-
-LOCK TABLES `produto` WRITE;
-/*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-/*!40000 ALTER TABLE `produto` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -90,18 +74,8 @@ CREATE TABLE `usuario` (
   `senha` varchar(32) NOT NULL,
   `tipo` enum('A','C') NOT NULL DEFAULT 'A',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `usuario`
---
-
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (1,'Administrador','admin','21232f297a57a5a743894a0e4a801fc3','A');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `venda`
@@ -114,23 +88,14 @@ CREATE TABLE `venda` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `data_venda` date NOT NULL,
   `prev_entrega` date NOT NULL,
-  `total_venda` double(9,2) unsigned NOT NULL,
+  `total` double(9,2) unsigned NOT NULL,
   `status_venda` varchar(60) NOT NULL,
-  `vendedor_matricula` int(10) unsigned NOT NULL,
+  `vendedor_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `vendedor_matricula` (`vendedor_matricula`),
-  CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`vendedor_matricula`) REFERENCES `vendedor` (`matricula`)
+  KEY `vendedor_id` (`vendedor_id`),
+  CONSTRAINT `venda_ibfk_1` FOREIGN KEY (`vendedor_id`) REFERENCES `vendedor` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `venda`
---
-
-LOCK TABLES `venda` WRITE;
-/*!40000 ALTER TABLE `venda` DISABLE KEYS */;
-/*!40000 ALTER TABLE `venda` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `vendedor`
@@ -140,23 +105,14 @@ DROP TABLE IF EXISTS `vendedor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `vendedor` (
-  `matricula` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `matricula` int(10) unsigned NOT NULL,
   `nome` varchar(60) NOT NULL,
   `dtnasc` date NOT NULL,
   `comissao` double(9,2) unsigned NOT NULL,
-  PRIMARY KEY (`matricula`)
-) ENGINE=InnoDB AUTO_INCREMENT=1231231232 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `vendedor`
---
-
-LOCK TABLES `vendedor` WRITE;
-/*!40000 ALTER TABLE `vendedor` DISABLE KEYS */;
-INSERT INTO `vendedor` VALUES (1231231231,'teste','2014-06-04',5.00);
-/*!40000 ALTER TABLE `vendedor` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -167,4 +123,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-06-05  3:41:28
+-- Dump completed on 2014-06-30 15:17:27
